ab9eb54c780fe83c0e5b44b2a3ce4873deedb8b7
otteru <otteru@naver.com>
2024-08-31 16:48:59 +0900

    섹션 3 완료

--- src/main/java/hello/itemservice/domain/item/ItemType.java
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -1,5 +1,7 @@
 package hello.itemservice.domain.item;
 
+import lombok.Getter;
+
 public enum ItemType {
     Book("도서"), FOOD("음식"), ETC("기타");
 
@@ -7,5 +9,9 @@ public enum ItemType {
 
     private ItemType(String description) {
         this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
